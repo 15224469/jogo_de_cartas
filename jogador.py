b2c9d8572e65c120af560ac0599b8b0fcf5b625d
15224469 <brendarafaely33@gmail.com>
2024-03-21 17:48:01 -0300

    Criei a classe jogador

---
+++ jogador.py
@@ -0,0 +1,12 @@
+class Jogador:
+    def __init__(self,nome):
+        self.nome = nome 
+        self.mao = []
+
+def receber_cartas(self, carta):
+    self.mao.append(carta)
+
+def mostrar_mao(self):
+    print ('{self.nome} tem as seguintas cartas na mão. ')
+    for carta in self.mao:
+        print (carta)
